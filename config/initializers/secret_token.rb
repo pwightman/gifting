a8048ca129bbf33332e9496ecd1304cd11f13b66
Parker Wightman <parkerwightman@gmail.com>
2012-12-13 20:23:18 -0700

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Gifting::Application.config.secret_token = '1ab208ebb2a102532222aac1c192157e362a8b5a507d22d38c2e50c998409c0439ba7f5b07a48850633aba7fcbfe3f5b0907c6065eb42d083df373458e65def7'
